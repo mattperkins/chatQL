a66da6f14acccbac583dbe0125e02a212cfa9ffe
mattperkins <github@mattperkins.com>
2018-11-13 21:35:06 +0000

    init express server

--- index.js
+++ index.js
@@ -2,48 +2,47 @@ const express = require('express')
 
 const app = express()
 const PORT = 3000
-  app.listen(PORT, () => console.log(`running on ${PORT}`);
- )
+  app.listen(PORT, () => console.log(`running on ${PORT}`))
 
-const { graphql, buildSchema } = require('graphql')
+// const { graphql, buildSchema } = require('graphql')
 
-const testDb = {
- users: [
-  { id: '1', email: 'fred@email.com', name: 'Fred'},
-  { id: '2', email: 'sandy@email.com', name: 'Sandy'}
- ]
-}
+// const testDb = {
+//  users: [
+//   { id: '1', email: 'fred@email.com', name: 'Fred'},
+//   { id: '2', email: 'sandy@email.com', name: 'Sandy'}
+//  ]
+// }
 
-const schema = buildSchema(`
- type Query {
-  users: [User!]!
- }
+// const schema = buildSchema(`
+//  type Query {
+//   users: [User!]!
+//  }
 
- type User {
-  id: ID!
-  email: String!
-  name: String
-  avatarUrl: String
- }
-`)
+//  type User {
+//   id: ID!
+//   email: String!
+//   name: String
+//   avatarUrl: String
+//  }
+// `)
 
-const rootValue = {
- users: () => testDb.users
-}
+// const rootValue = {
+//  users: () => testDb.users
+// }
 
-graphql(
- schema,
- `
-  {
-   users {
-    id
-    email
-   }
-  }
- `,
- rootValue
-).then(
- res => console.dir(res, { depth: null })
-).catch(
- console.error
-)
+// graphql(
+//  schema,
+//  `
+//   {
+//    users {
+//     id
+//     email
+//    }
+//   }
+//  `,
+//  rootValue
+// ).then(
+//  res => console.dir(res, { depth: null })
+// ).catch(
+//  console.error
+// )
