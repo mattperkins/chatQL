42af717dc19948033f9a8a7cf51b0ea4b276f16c
mattperkins <github@mattperkins.com>
2018-11-13 21:33:18 +0000

    init express server

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+  app.listen(PORT, () => console.log(`running on ${PORT}`);
+ )
+
 const { graphql, buildSchema } = require('graphql')
 
 const testDb = {
