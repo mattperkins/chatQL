c28f159d0d5a9789f06c10e68375b7306e176494
mattperkins <github@mattperkins.com>
2018-11-13 21:43:40 +0000

    add middleware and configure for graphiql

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 const express = require('express')
+const graphQlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+const testDb = {
+ users: [
+  { id: '1', email: 'fred@email.com', name: 'Fred'},
+  { id: '2', email: 'sandy@email.com', name: 'Sandy'}
+ ]
+}
+
+const schema = buildSchema(`
+ type Query {
+  users: [User!]!
+ }
+
+ type User {
+  id: ID!
+  email: String!
+  name: String
+  avatarUrl: String
+ }
+`)
+
+const rootValue = {
+ users: () => testDb.users
+}
 
 const app = express()
+
+// middleware
+app.use('/graphql', graphQlHTTP({
+ schema,
+ rootValue,
+ graphiql: true
+}))
+
 const PORT = 3000
   app.listen(PORT, () => console.log(`running on ${PORT}`))
 
 // const { graphql, buildSchema } = require('graphql')
-
-// const testDb = {
-//  users: [
-//   { id: '1', email: 'fred@email.com', name: 'Fred'},
-//   { id: '2', email: 'sandy@email.com', name: 'Sandy'}
-//  ]
-// }
-
-// const schema = buildSchema(`
-//  type Query {
-//   users: [User!]!
-//  }
-
-//  type User {
-//   id: ID!
-//   email: String!
-//   name: String
-//   avatarUrl: String
-//  }
-// `)
-
-// const rootValue = {
-//  users: () => testDb.users
-// }
 
 // graphql(
 //  schema,
