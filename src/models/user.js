4c07509e9abc123d396c411c7d03cfe055c0334f
mattperkins <github@mattperkins.com>
2018-11-17 16:11:15 +0000

    use timestamp instead of createdAt < schema definitions order !important

--- src/models/user.js
+++ src/models/user.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
   email: String,
   username: String,
-  name: String,
-  createdAt: String
+  name: String
+}, {
+  timestamps: true
 })
 
 export default mongoose.model('User', userSchema)
