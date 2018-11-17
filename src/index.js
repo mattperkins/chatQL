b1fe08fbcf757dd02dbd55b38fde313e50548cbd
mattperkins <github@mattperkins.com>
2018-11-17 13:45:12 +0000

    configure graphql playground for development

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ const {
   NODE_ENV = 'development'
 } = process.env
 
+const IN_PROD = NODE_ENV === 'production'
+
 const app = express()
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  playground: !IN_PROD
 })
 
 // app is from an existing express app
