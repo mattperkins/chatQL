8ee26e37f9e1149c2e4b79112a545867c599e314
mattperkins <github@mattperkins.com>
2018-11-17 15:56:07 +0000

    update server launch confirmation message

--- src/index.js
+++ src/index.js
@@ -5,25 +5,32 @@ import typeDefs from './typeDefs'
 import resolvers from './resolvers'
 import { APP_PORT, IN_PROD, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } from '../env.js'
 
-mongoose.connect(
-  `
+// self invoking async function
+(async () => {
+  try {
+    await mongoose.connect(
+      `
   mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
   `,
-  { useNewUrlParser: true }
-)
+      { useNewUrlParser: true }
+    )
 
-const app = express()
-app.disable('x-powered-by')
+    const app = express()
+    app.disable('x-powered-by')
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  playground: !IN_PROD
-})
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      playground: !IN_PROD
+    })
 
-// app is from an existing express app
-server.applyMiddleware({ app })
+    // app is from an existing express app
+    server.applyMiddleware({ app })
 
-app.listen({ port: APP_PORT }, () =>
-  console.log(`GQL Playground: http://localhost:${APP_PORT}${server.graphqlPath}`)
-)
+    app.listen({ port: APP_PORT }, () =>
+      console.log(`GQL Playground up @ http://localhost:${APP_PORT}${server.graphqlPath}`)
+    )
+  } catch (e) {
+    console.log(e)
+  }
+})()
