1c9148dfd92b5f124b1fca5b49c2ed9704735bc9
mattperkins <github@mattperkins.com>
2018-11-17 12:41:38 +0000

    add apollo server via express middleware

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const { ApolloServer, gql } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema');
+
+const server = new ApolloServer({
+  // These will be defined for both new or existing servers
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app }); // app is from an existing express app
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
