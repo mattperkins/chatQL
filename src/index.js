d2528637ae071345e9c4847a19025cece5ff8de8
mattperkins <github@mattperkins.com>
2018-11-17 12:45:19 +0000

    refactor => remove comments and fix formatting

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import { ApolloServer, gql } from 'apollo-server-express'
 import { typeDefs, resolvers } from './schema'
 
 const server = new ApolloServer({
-  // These will be defined for both new or existing servers
   typeDefs,
   resolvers,
 });
 
-server.applyMiddleware({ app }); // app is from an existing express app
+// app is from an existing express app
+server.applyMiddleware({ app }); 
 
 app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  console.log(`Server endpoint: http://localhost:4000${server.graphqlPath}`)
 )
