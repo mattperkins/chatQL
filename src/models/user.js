96d4a76aea870c4fffc741a180f834bd57720b1f
mattperkins <github@mattperkins.com>
2018-11-18 10:24:02 +0000

    configure user schema

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
   email: String,
   username: String,
-  name: String
+  name: String,
+  // stores password in DB not exposing to graphql API
+  password: String
 }, {
 // createdAt and updatedAt implicit when model updates
   timestamps: true
