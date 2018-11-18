ee075303de0db9090f303997be593ba6038db735
mattperkins <github@mattperkins.com>
2018-11-18 10:39:06 +0000

    add mongoose utlity method conditional check id on single user resolver and apollo error

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,14 +1,19 @@
+import mongoose from 'mongoose'
+import { UserInputError } from 'apollo-server-express'
 import { User } from '../models'
 
 export default {
   Query: {
     users: (root, arg, context, info) => {
-      // Auth, Projection, Pagination
+      // Auth, Projection, Pagination, Sanitisation
 
       return User.find({})
     },
-    user: (root, arg, context, info) => {
-
+    user: (root, { id }, context, info) => {
+      // Utility method
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        throw new UserInputError(`${id} is not a valid user ID.`)
+      }
     }
   },
   Mutation: {
