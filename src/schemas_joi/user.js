05b427170e36fdbc569d55163e76dd20fcef771d
mattperkins <github@mattperkins.com>
2018-11-18 21:18:25 +0000

    add regex validation message option

--- src/schemas_joi/user.js
+++ src/schemas_joi/user.js
@@ -2,8 +2,16 @@ import Joi from 'joi'
 
 // object schema definition
 export default Joi.object().keys({
-  email: Joi.string().email.required().label('Email'),
+  email: Joi.string().email().required().label('Email'),
   username: Joi.string().alphanum().min(4).max(50).required().label('Username'),
   name: Joi.string().max(254).required().label('Name'),
-  password: Joi.string().regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,30}$/)
+  password: Joi.string().regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,30}$/).label('Password').options({
+    language: {
+      string: {
+        regex: {
+          base: 'Must have at least on lowercase letter, oneupper case letter, one digit and one specical character'
+        }
+      }
+    }
+  })
 })
