142fa006e2b85b6461ac2e25c634cb4aabd2e1c1
mattperkins <github@mattperkins.com>
2018-11-19 00:08:35 +0000

    refactor character limit on username to 3

--- src/schemas_joi/user.js
+++ src/schemas_joi/user.js
@@ -3,7 +3,7 @@ import Joi from 'joi'
 // object schema definition
 export default Joi.object().keys({
   email: Joi.string().email().required().label('Email'),
-  username: Joi.string().alphanum().min(4).max(50).required().label('Username'),
+  username: Joi.string().alphanum().min(3).max(50).required().label('Username'),
   name: Joi.string().max(254).required().label('Name'),
   password: Joi.string().regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,30}$/).label('Password').options({
     language: {
