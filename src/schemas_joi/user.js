bffa7f20f573d0fffa33e2b9bfb55ce32e2d741c
mattperkins <github@mattperkins.com>
2018-12-10 19:44:53 +0000

    add min max Joi validation for password

--- src/schemas_joi/user.js
+++ src/schemas_joi/user.js
@@ -5,7 +5,7 @@ export default Joi.object().keys({
   email: Joi.string().email().required().label('Email'),
   username: Joi.string().alphanum().min(3).max(50).required().label('Username'),
   name: Joi.string().max(254).required().label('Name'),
-  password: Joi.string().regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,30}$/).label('Password').options({
+  password: Joi.string().min(8).max(30).regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,30}$/).label('Password').options({
     language: {
       string: {
         regex: {
