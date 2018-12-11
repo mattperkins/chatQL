b8a12446047b22f863a18492217e59a105d74031
mattperkins <github@mattperkins.com>
2018-12-11 03:13:17 +0000

    simplify the regex joi validator > remove need for special character/add Joi character limit instead of regex check

--- src/schemas_joi/user.js
+++ src/schemas_joi/user.js
@@ -5,11 +5,11 @@ export default Joi.object().keys({
   email: Joi.string().email().required().label('Email'),
   username: Joi.string().alphanum().min(3).max(50).required().label('Username'),
   name: Joi.string().max(254).required().label('Name'),
-  password: Joi.string().min(8).max(30).regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,30}$/).label('Password').options({
+  password: Joi.string().min(8).max(30).regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d).*$/).required().label('Password').options({
     language: {
       string: {
         regex: {
-          base: 'Must have at least one lowercase letter, one uppercase letter, one digit and one special character'
+          base: 'Must have at least one lowercase letter, one uppercase letter and one digit'
         }
       }
     }
